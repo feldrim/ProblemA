656ac4118b391ad3003fa14200b95608e9c379fb
the f <feldrim@gmail.com>
2018-04-12 22:16:44 +0300

    Added all 4 tests for sample data

--- ProblemA.Tests/ProblemATests.cs
+++ ProblemA.Tests/ProblemATests.cs
@@ -9,10 +9,45 @@ namespace ProblemA.Tests
       [TestMethod]
       public void SampleInputOneReturnsTrue()
       {
-         var measurements = new List<Measurement> { new Measurement(0, 1, 1), new Measurement(1, 0, 0) };
          const int capacity = 1;
-         var checker = new CapacityChecker(capacity);
-         Assert.IsTrue(checker.IsPossibble(measurements));
+         const int numberOfStations = 2;
+         var measurements = new List<Measurement> { new Measurement(0, 1, 1), new Measurement(1, 0, 0) };
+         var checker = new CapacityChecker(capacity, numberOfStations, measurements);
+
+         Assert.IsTrue(checker.IsPossibble());
+      }
+
+      [TestMethod]
+      public void SampleInputTwoReturnsFalse()
+      {
+         const int capacity = 1;
+         const int numberOfStations = 2;
+         var measurements = new List<Measurement> { new Measurement(1, 0, 0), new Measurement(0, 1, 0) };
+         var checker = new CapacityChecker(capacity, numberOfStations, measurements);
+
+         Assert.IsFalse(checker.IsPossibble());
+      }
+
+      [TestMethod]
+      public void SampleInputThreeReturnsFalse()
+      {
+         const int capacity = 1;
+         const int numberOfStations = 2;
+         var measurements = new List<Measurement> { new Measurement(0, 1, 0), new Measurement(1, 0, 1) };
+         var checker = new CapacityChecker(capacity, numberOfStations, measurements);
+
+         Assert.IsFalse(checker.IsPossibble());
+      }
+
+      [TestMethod]
+      public void SampleInputFourReturnsFalse()
+      {
+         const int capacity = 1;
+         const int numberOfStations = 2;
+         var measurements = new List<Measurement> { new Measurement(0, 1, 1), new Measurement(0, 0, 0) };
+         var checker = new CapacityChecker(capacity, numberOfStations, measurements);
+
+         Assert.IsFalse(checker.IsPossibble());
       }
    }
 }
